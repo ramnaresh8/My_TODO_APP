669890c6b220a625242a0e10f09eafa88472ba5e
Ram <ramnareshkushwah823@gmail.com>
2024-05-04 14:21:04 +0530

    Initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+## Todo App
+
+- Anyone can create a todo
+- Anyone can see their existing todo
+- Anyone can mark their todo as done
---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,10 @@
+export function Createtodo(){
+    return(
+        <div>
+            <input type="text" placeholder="title" /> <br />
+            <input type="text" placeholder="description" /><br />
+
+            <button>Add a todo</button>
+        </div>
+    )
+}
